fcca6939f88ca2ae1136e025bd85368cd42a389c
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-04-17 16:11:18 +0100

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const expirationSelect = document.querySelector('[data-expiration-year]')
+const logo = document.querySelector('[data-logo]')
 
 const currentYear = new Date().getFullYear()
 for(let i = currentYear; i < currentYear + 10; i++) {
@@ -11,7 +12,7 @@ for(let i = currentYear; i < currentYear + 10; i++) {
 document.addEventListener('keydown', e => {
     const input = e.target
     const key = e.key
-    if(!isConnetedInput(input)) return
+    if(!isConnectedInput(input)) return
 
     switch (key) {
         case "ArrowLeft": {
@@ -34,8 +35,31 @@ document.addEventListener('keydown', e => {
             }
             break
         }
+        case 'Delete': {
+            if (input.selectionStart === input.value.length && input.selectionEnd === input.value.length) {
+                const next = input.nextElementSibling
+                next.value = next.value.substring(1, next.value.length )
+                next.focus()
+                next.selectionStart = 0
+                next.selectionEnd = 0
+                e.preventDefault()
+            }
+            break
+        }
+        case 'Backspace': {
+            if (input.selectionStart === 0 && input.selectionEnd === 0) {
+                const prev = input.previousElementSibling
+                prev.value = prev.value.substring(0, prev.value.length -1)
+                prev.focus()
+                prev.selectionStart = prev.value.length
+                prev.selectionEnd = prev.value.length
+                e.preventDefault()
+            }
+            break
+        }
         default: {
             if (e.ctrlKey || e.altKey) return
+            if (key.length > 1) return 
             if (key.match(/^[^0-9]$/)) return e.preventDefault()
 
             e.preventDefault()
@@ -44,11 +68,31 @@ document.addEventListener('keydown', e => {
     }
 })
 
+document.addEventListener('paste', e => {
+    const input = e.target
+    const data = e.clipBoardData.getData('text')
+
+    if(!isConnectedInput(input)) return
+    if(!data.match(/^[0-9]+$/)) return e.preventDefault()
+
+    e.preventDefault()
+    onInputChange(input, data)
+})
+
 function onInputChange (input, newValue) {
     const start = input.selectionStart
     const end = input.selectionEnd
     updateInputValue(input, newValue, start, end)
-    focusInput()
+    focusInput(input, newValue.length + start )
+    const firstFour = input
+    .closest('[data-connected-inputs]')
+    .querySelector('input').value
+
+    if(firstFour.startsWith('4')) {
+        logo.src = 'visa.svg'
+    } else if (firstFour.startsWith('5')) {
+        logo.src = 'mastercard.svg'
+    }
 }
 
 
@@ -62,7 +106,21 @@ function updateInputValue (input, extraValue, start = 0, end = 0) {
     }
 }
 
-function isConnetedInput(input) {
+function focusInput (input, dataLength) {
+    let addedChars = dataLength
+    let currrentInput = input
+    while(addedChars > 4 && currrentInput.nextElementSibling != null ) {
+        addedChars -= 4
+        currrentInput = currrentInput.nextElementSibling
+    }
+    if(addedChars > 4) addedChars = 4
+
+    currrentInput.focus()
+    currrentInput.selectionStart = addedChars
+    currrentInput.selectionEnd = addedChars
+}
+
+function isConnectedInput(input) {
     const parent = input.closest('[data-connected-inputs]')
     return input.matches('input') && parent != null
 }
